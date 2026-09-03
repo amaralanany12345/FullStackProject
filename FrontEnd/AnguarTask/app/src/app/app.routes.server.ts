f050ffdf28ca8b539e5e45fc45e5fc903a896267
amaralanany12345 <Alanany09@gmail.com>
2026-09-03 12:21:56 +0300

    Apply the SKIP_AUTH for services that don't need for authentication and apply the SSR , CSR and PreRendering fot each route with its suitable situation

--- FrontEnd/AnguarTask/app/src/app/app.routes.server.ts
+++ FrontEnd/AnguarTask/app/src/app/app.routes.server.ts
@@ -2,13 +2,53 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home/items/:id',
     renderMode: RenderMode.Server
   },
-  // {
-  //   path: 'home',
-  //   renderMode: RenderMode.Client
-  // },
+  {
+    path: 'home/orders',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home/orders/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'home/receipts',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home/receipts/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'home/activeOrder/:orderId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'home/updateItem/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'home/createCategory',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home/createItem',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home/paymentPage/:orderId',
+    renderMode: RenderMode.Server
+  },
   
 
 ];
--- FrontEnd/AnguarTask/app/src/app/app.routes.ts
+++ FrontEnd/AnguarTask/app/src/app/app.routes.ts
@@ -1,18 +1,4 @@
 import { Routes } from '@angular/router';
-import { SigninComponent } from './Components/signin-component/signin-component';
-import { HomeComponent } from './Components/home-component/home-component';
-import { ItemDetails } from './Components/item-details/item-details';
-import { AllOrders } from './Components/all-orders/all-orders';
-import { OrderDetails } from './Components/order-details/order-details';
-import { UpdateItem } from './Components/update-item/update-item';
-import { CreateCategory } from './Components/create-category/create-category';
-import { AllReceiptsComponent } from './Components/all-receipts-component/all-receipts-component';
-import { CreateItem } from './Components/create-item/create-item';
-import { ReceiptComponent } from './Components/receipt-component/receipt-component';
-import { PaymentPage } from './Components/payment-page/payment-page';
-import { RefreshTokenComponent } from './Components/refresh-token-component/refresh-token-component';
-import { ActiveOrder } from './Components/active-order/active-order';
-
 
 export const routes: Routes = [
     {
